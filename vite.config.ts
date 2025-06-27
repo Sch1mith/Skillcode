021aecd5321a0c56df1f7de47ff19b9cedf2b9cb
ikuuro <ikaro.stos.olvr@gmail.com>
2025-06-27 03:59:24 -0300

    funcional

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',       // Permite acesso externo
+    host: 'localhost', // Restringe acesso apenas ao desktop/local
     port: 5173,            // Porta padrão do Vite
     proxy: {
       '/api': {
